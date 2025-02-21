e7e8d050bb24b4936baad14e63e17eb6a85ab960
Rodsone Fernandes <rodsonfernandes03@gmail.com>
2025-02-21 22:58:50 +0200

    first commit with structure

---
+++ src/app/layout.js
@@ -0,0 +1,30 @@
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500'],
+  variable: '--font-poppins',
+});
+
+
+export const metadata = {
+  title: "Blog",
+  description: "Frontpush 3.0 blog",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${poppins.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
+
+
+
+
---
+++ src/components/Posts/index.js
@@ -0,0 +1,9 @@
+
+
+export default function Post(){
+    return(
+        <div>
+            header
+        </div>
+    )
+}
